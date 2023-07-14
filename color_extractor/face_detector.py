947d1df056ae62f838e6e2133a23405c77fb6774
flyahn06 <flyahn06@gmail.com>
2023-07-14 22:44:47 +0900

    face_detector.py 작성 완료 get_part() 완료 디버그용 _show_points(), _show_entire_points() 코드 작성 FACIAL_LANDMARKS_IDXS_CHEEK 미세 조정 테스트용 코드 작성

--- color_extractor/face_detector.py
+++ color_extractor/face_detector.py
@@ -1,3 +1,9 @@
+# +------------+--------------+-----------------------------------------------------------+
+# |   Author   |     Date     |                         Changed                           |
+# +------------+--------------+-----------------------------------------------------------+
+# |  Andrew A. |  2023/07/14  | Initial release                                           |
+# +------------+--------------+-----------------------------------------------------------+
+
 import copy
 from collections import OrderedDict
 from imutils import face_utils
@@ -6,77 +12,93 @@ import imutils
 import dlib
 import cv2
 
+# 재정의: 필요한 부분만 뽑음
+# 형식: (이름, (모델 포인트 기준 인덱스 시작점, 모델 포인트 기준 인덱스 끝점))
 FACIAL_LANDMARKS_IDXS = OrderedDict([
     ("nose", (27, 35)),
     ("jaw", (0, 17))
 ])
 
+# 형식: (이름, (모델 포인트 기준 좌표*))
 FACIAL_LANDMARKS_IDXS_CHEEK = OrderedDict([
-    ("left_cheek", (37, 2, 3, 4, 49, 32)),
-    ("right_cheek", (46, 16, 15, 14, 55, 36))
+    ("left_cheek", (1, 2, 3, 4, 31, 36)),
+    ("right_cheek", (16, 15, 14, 13, 35, 45))
 ])
 
 
 class FacePart:
     def __init__(self, image_path, predictor_path):
+        # 인식된 얼굴 부분의 좌표가 들어갈 사전
         self._facial_marks = dict()
+        # 인식할 부분
         self._available_parts = ["nose", "jaw", "left_cheek", "right_cheek"]
 
         detector = dlib.get_frontal_face_detector()
-        predictor = dlib.shape_predictor(predictor_path)
+        self.predictor = dlib.shape_predictor(predictor_path)
 
-        image = cv2.imread(image_path)
-        image = imutils.resize(image, width=500)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # 여기서 얼굴이 인식됨 (rects는 얼굴이 있는 좌표 집합)
+        self.image = cv2.imread(image_path)
+        self.image = imutils.resize(self.image, width=500)
+        self.gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        # 여기서 얼굴이 인식됩니다. (rects는 얼굴이 있는 좌표 집합)
         # type: dlib.rectanges(dlib.rectangle)
-        rects = detector(gray, 1)
+        self.rects = detector(self.gray, 1)  # 여러 얼굴 처리... TODO: 한 얼굴 처리로 바꾸기
 
-        # TODO: 테스트
-        # rect = rects[0]
-        # cv2.rectangle(image, (rect.tl_corner().x, rect.tl_corner().y), (rect.br_corner().x, rect.br_corner().y), (0, 0, 255))
-        # cv2.imshow("face", image)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-
-        for (point_index_start, rect) in enumerate(rects):
-            shape = predictor(gray, rect)
+        for (i, rect) in enumerate(self.rects):  # 필요 없는 루프
+            shape = self.predictor(self.gray, rect)
             shape = face_utils.shape_to_np(shape)
+            # shape 형식: https://pyimagesearch.com/wp-content/uploads/2017/04/facial_landmarks_68markup.jpg
 
+            # FACIAL_LANDMARKS_IDXS_CHEEK, FACIAL_LANDMARKS_IDXS 형식이 다르므로
+            # 두 개로 나눠서 처리함
+
+            # FACIAL_LANDMARKS_IDXS 처리
             for (name, (point_index_start, point_index_end)) in FACIAL_LANDMARKS_IDXS.items():
-                # i는 인덱스 시작점, j는 인덱스 끝점
-                # 참고: https://pyimagesearch.com/wp-content/uploads/2017/04/facial_landmarks_68markup.jpg
                 temp = []
                 for x, y in shape[point_index_start:point_index_end]:
-                    temp.append((x, y))
+                    temp.append([x, y])
                 # 리스트 참조를 끊기 위해 깊은 복사 사용
+                temp = np.array(temp)
                 self._facial_marks[name] = copy.deepcopy(temp)
 
+            # FACIAL_LANDMARKS_IDXS_CHEEK 처리
             for name, points in FACIAL_LANDMARKS_IDXS_CHEEK.items():
-                # 여기서는 사용할 점들을 튜플로 표시합니다.
-
                 temp = []
                 for point in points:
-                    temp.append(((p := shape[point])[0], p[1]))
-
+                    temp.append([(p := shape[point])[0], p[1]])
+                temp = np.array(temp)
                 self._facial_marks[name] = copy.deepcopy(temp)
 
-
+    def _show_points(self):
+        clone = self.image.copy()
         for name, pointlist in self._facial_marks.items():
             for point in pointlist:
                 if name in ("nose", "jaw"):
-                    cv2.circle(image, point, 3, (255, 0, 0))
-                    pass
+                    cv2.circle(clone, point, 3, (255, 0, 0))
                 else:
-                    cv2.circle(image, point, 1, (255, 255, 0))
 
-        cv2.putText(image, "nose", self._facial_marks["nose"][0], cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-        cv2.putText(image, "jaw", self._facial_marks["jaw"][0], cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-        cv2.putText(image, "left_cheek", self._facial_marks["left_cheek"][0], cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(image, "right_cheek", self._facial_marks["right_cheek"][0], cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    cv2.circle(clone, point, 1, (255, 255, 0))
+
+        cv2.putText(clone, "nose", self._facial_marks["nose"][0], cv2.FONT_HERSHEY_SIMPLEX, 0.1, (255, 255, 255), 2)
+        cv2.putText(clone, "jaw", self._facial_marks["jaw"][0], cv2.FONT_HERSHEY_SIMPLEX, 0.1, (255, 255, 255), 2)
+        cv2.putText(clone, "left_cheek", self._facial_marks["left_cheek"][0], cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), 1)
+        cv2.putText(clone, "right_cheek", self._facial_marks["right_cheek"][0], cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255), 1)
 
 
-        cv2.imshow("face", image)
+        cv2.imshow("face", clone)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def _show_entire_points(self):
+        clone = self.image.copy()
+
+        shape = self.predictor(self.gray, self.rects[0])
+        shape = face_utils.shape_to_np(shape)
+
+        for i, (x, y) in enumerate(shape):
+            cv2.circle(clone, (x, y), 1, (0, 0, 255))
+            cv2.putText(clone, str(i), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 1)
+
+        cv2.imshow("face", clone)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
@@ -95,8 +117,24 @@ class FacePart:
         if part not in self._available_parts:
             raise ValueError("{} is not available.".format(part))
 
-        # TODO: cv2 이미지 리턴
+        print(self._facial_marks[part])
+        x, y, w, h = cv2.boundingRect(self._facial_marks[part])
+        crop = self.image[y:y+h, x:x+w]
+        adj_points = np.array([np.array([p[0]-x, p[1]-y]) for p in self._facial_marks[part]])
+
+        # 마스크 설정
+        mask = np.zeros((crop.shape[0], crop.shape[1]))
+        cv2.fillConvexPoly(mask, adj_points, 1)
+        mask = mask.astype(np.bool_)
+        crop[np.logical_not(mask)] = [255, 0, 0]
+
+        cv2.imshow(part, crop)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    face = FacePart("../test/face.png", "../resources/shape_predictor_68_face_landmarks.dat")
+    face = FacePart("../test/face2.png", "../resources/shape_predictor_68_face_landmarks.dat")
+    face._show_entire_points()
+    for p in face._available_parts:
+        face.get_part(p)
